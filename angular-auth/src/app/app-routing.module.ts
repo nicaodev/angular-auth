85295bc0f51fe9601dfc619ab7822bf26c37ba79
nicaodev <alexandre.nicos@gmail.com>
2022-12-26 22:37:49 -0300

    lazy loading nas rotas

--- angular-auth/src/app/app-routing.module.ts
+++ angular-auth/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./core/componentes/auth/auth.module').then(m => m.AuthModule), //Lazy loading
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./componentes/admin/admin.module').then(a => a.AdminModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
